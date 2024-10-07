f32afdec75ed0dc97e00a6782b8174d829c73cc4
kevinlizandrousccausca <kevin.uscca@tecsup.edu.pe>
2024-10-07 17:55:14 -0500

    Chore: ScreenSeries

--- app/src/main/java/com/example/lab10/ScreenSeries.kt
+++ app/src/main/java/com/example/lab10/ScreenSeries.kt
@@ -36,4 +36,70 @@ import com.tecsup.lab10.data.SerieModel
 import kotlinx.coroutines.delay
 
 
+@Composable
+fun ContenidoSeriesListado(navController: NavHostController, servicio: SerieApiService) {
+    var listaSeries: SnapshotStateList<SerieModel> = remember { mutableStateListOf() }
+    LaunchedEffect(Unit) {
+        val listado = servicio.selectSeries()
+        listado.forEach { listaSeries.add(it) }
+    }
+
+    LazyColumn (
+
+    ){
+        item {
+            Row (
+                modifier = Modifier.fillParentMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "ID",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(0.1f)
+                )
+                Text(
+                    text = "SERIE",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(0.7f)
+                )
+                Text(
+                    text = "Accion",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(0.2f)
+                ) //, fontWeight = FontWeight.Bold)
+            }
+        }
+
+        items(listaSeries) { item ->
+            Row(
+                modifier = Modifier.padding(start=8.dp).fillParentMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "${item.id}", fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier=Modifier.weight(0.1f))
+                Text(text = item.name, fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier=Modifier.weight(0.6f))
+                IconButton(
+                    onClick = {
+                        navController.navigate("serieVer/${item.id}")
+                        Log.e("SERIE-VER","ID = ${item.id}")
+                    },
+                    Modifier.weight(0.1f)
+                ) {
+                    Icon(imageVector = Icons.Outlined.Edit, contentDescription = "Ver", modifier=Modifier.align(Alignment.CenterVertically))
+                }
+                IconButton(
+                    onClick = {
+                        navController.navigate("serieDel/${item.id}")
+                        Log.e("SERIE-DEL","ID = ${item.id}")
+                    },
+                    Modifier.weight(0.1f)
+                ) {
+                    Icon(imageVector = Icons.Outlined.Delete, contentDescription = "Ver", modifier=Modifier.align(Alignment.CenterVertically))
+                }
+            }
+        }
+    }
+}
 
