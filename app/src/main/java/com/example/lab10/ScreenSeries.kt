9ae292bd3d6caa2bdc30c83b97a185b0fd57d05e
kevinlizandrousccausca <kevin.uscca@tecsup.edu.pe>
2024-10-07 17:57:26 -0500

    Chore: ScreenSeries-editar

--- app/src/main/java/com/example/lab10/ScreenSeries.kt
+++ app/src/main/java/com/example/lab10/ScreenSeries.kt
@@ -103,3 +103,83 @@ fun ContenidoSeriesListado(navController: NavHostController, servicio: SerieApiS
     }
 }
 
+@Composable
+fun ContenidoSerieEditar(navController: NavHostController, servicio: SerieApiService, pid: Int = 0 ) {
+    var id by remember { mutableStateOf<Int>(pid) }
+    var name by remember { mutableStateOf<String?>("") }
+    var release_date by remember { mutableStateOf<String?>("") }
+    var rating by remember { mutableStateOf<String?>("") }
+    var category by remember { mutableStateOf<String?>("") }
+    var grabar by remember { mutableStateOf(false) }
+
+    if (id != 0) {
+        LaunchedEffect(Unit) {
+            val objSerie = servicio.selectSerie(id.toString())
+            delay(100)
+            name = objSerie.body()?.name
+            release_date = objSerie.body()?.release_date
+            rating = objSerie.body()?.rating.toString()
+            category = objSerie.body()?.category
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        // Spacer(Modifier.height(50.dp))
+        TextField(
+            value = id.toString(),
+            onValueChange = { },
+            label = { Text("ID (solo lectura)") },
+            readOnly = true,
+            singleLine = true
+        )
+        TextField(
+            value = name!!,
+            onValueChange = { name = it },
+            label = { Text("Name: ") },
+            singleLine = true
+        )
+        TextField(
+            value = release_date!!,
+            onValueChange = { release_date = it },
+            label = { Text("Release Date:") },
+            singleLine = true
+        )
+        TextField(
+            value = rating!!,
+            onValueChange = { rating = it },
+            label = { Text("Rating:") },
+            singleLine = true
+        )
+        TextField(
+            value = category!!,
+            onValueChange = { category = it },
+            label = { Text("Category:") },
+            singleLine = true
+        )
+        Button(
+            onClick = {
+                grabar = true
+            }
+        ) {
+            Text("Grabar", fontSize=16.sp)
+        }
+    }
+
+    if (grabar) {
+        val objSerie = SerieModel(id,name!!, release_date!!, rating!!.toInt(), category!!)
+        LaunchedEffect(Unit) {
+            if (id == 0)
+                servicio.insertSerie(objSerie)
+            else
+                servicio.updateSerie(id.toString(), objSerie)
+        }
+        grabar = false
+        navController.navigate("series")
+    }
+}
+
+
