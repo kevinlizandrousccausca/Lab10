2d3d05cd25212a0e86537d733a278ab4b426a599
kevinlizandrousccausca <kevin.uscca@tecsup.edu.pe>
2024-10-07 18:00:22 -0500

    Chore: ScreenSeries-eliminar

--- app/src/main/java/com/example/lab10/ScreenSeries.kt
+++ app/src/main/java/com/example/lab10/ScreenSeries.kt
@@ -182,4 +182,42 @@ fun ContenidoSerieEditar(navController: NavHostController, servicio: SerieApiSer
     }
 }
 
+@Composable
+fun ContenidoSerieEliminar(navController: NavHostController, servicio: SerieApiService, id: Int) {
+    var showDialog by remember { mutableStateOf(true) }
+    var borrar by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = "Confirmar Eliminación") },
+            text = {  Text("¿Está seguro de eliminar la Serie?") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        showDialog = false
+                        borrar = true
+                    } ) {
+                    Text("Aceptar")
+                }
+            },
+            dismissButton = {
+                Button( onClick = { showDialog = false } ) {
+                    Text("Cancelar")
+                    navController.navigate("series")
+                }
+            }
+        )
+    }
+    if (borrar) {
+        LaunchedEffect(Unit) {
+            // val objSerie = servicio.selectSerie(id.toString())
+            servicio.deleteSerie(id.toString())
+            borrar = false
+            navController.navigate("series")
+        }
+    }
+}
+
+
 
